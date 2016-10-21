ce65c7b31b43066232906151254d5afe4af0fcd2
Pedro Costa <pedrodc23@gmail.com>
2016-10-21 22:42:46 +0100

    Materials' inheritance implemented

--- reader/Component.js
+++ reader/Component.js
@@ -7,6 +7,7 @@ function Component(scene, transformations, materials, texture, childrenComponent
 
     this.texture = texture;
     this.materials = materials;
+    this.currentMaterial = this.materials[0];
     this.transformations = transformations;
     this.childrenComponent = childrenComponent;
     this.childrenPrimitive = childrenPrimitive;
@@ -17,6 +18,25 @@ function Component(scene, transformations, materials, texture, childrenComponent
         this.appearance.loadTexture(this.texture[1]);
 
 };
+
+Component.prototype.setMaterials = function(materials) {
+    this.materials = materials;
+    this.currentMaterial = this.materials[0];
+    this.appearance.setEmission(this.currentMaterial[0][0], this.currentMaterial[0][1], this.currentMaterial[0][2], this.currentMaterial[0][3]);
+    this.appearance.setAmbient(this.currentMaterial[1][0], this.currentMaterial[1][1], this.currentMaterial[1][2], this.currentMaterial[1][3]);
+    this.appearance.setDiffuse(this.currentMaterial[2][0], this.currentMaterial[2][1], this.currentMaterial[2][2], this.currentMaterial[2][3]);
+    this.appearance.setSpecular(this.currentMaterial[3][0], this.currentMaterial[3][1], this.currentMaterial[3][2], this.currentMaterial[3][3]);
+    this.appearance.setShininess(this.currentMaterial[4][0]);
+}
+
+Component.prototype.changeCurrentMaterial = function(index) {
+    this.currentMaterial = this.materials[index];
+    this.appearance.setEmission(this.currentMaterial[0][0], this.currentMaterial[0][1], this.currentMaterial[0][2], this.currentMaterial[0][3]);
+    this.appearance.setAmbient(this.currentMaterial[1][0], this.currentMaterial[1][1], this.currentMaterial[1][2], this.currentMaterial[1][3]);
+    this.appearance.setDiffuse(this.currentMaterial[2][0], this.currentMaterial[2][1], this.currentMaterial[2][2], this.currentMaterial[2][3]);
+    this.appearance.setSpecular(this.currentMaterial[3][0], this.currentMaterial[3][1], this.currentMaterial[3][2], this.currentMaterial[3][3]);
+    this.appearance.setShininess(this.currentMaterial[4][0]);
+}
 
 Component.prototype.getTexture = function() {
     return this.texture;
