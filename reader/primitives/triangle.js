aaad3fa0400b55cad91e12130242288525c7210f
Pedro Costa <pedrodc23@gmail.com>
2016-10-23 21:41:02 +0100

    updated triangle coords

--- reader/primitives/triangle.js
+++ reader/primitives/triangle.js
@@ -46,15 +46,17 @@ Triangle.prototype.initBuffers = function() {
         0, 0, 1
     ];
 
-    var ab = Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2) + Math.pow(this.z2 - this.z1, 2));
-    var bc = Math.sqrt(Math.pow(this.x2 - this.x3, 2) + Math.pow(this.y2 - this.y3, 2) + Math.pow(this.z2 - this.z3, 2));
-    var ac = Math.sqrt(Math.pow(this.x1 - this.x3, 2) + Math.pow(this.y1 - this.y3, 2) + Math.pow(this.z1 - this.z3, 2));
-    var beta = Math.acos((Math.pow(bc, 2) + Math.pow(ab, 2) - Math.pow(ac, 2)) / (2 * ab * bc));
+    var side1 = Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2) + Math.pow(this.z2 - this.z1, 2));
+    var side2 = Math.sqrt(Math.pow(this.x2 - this.x3, 2) + Math.pow(this.y2 - this.y3, 2) + Math.pow(this.z2 - this.z3, 2));
+    var side3 = Math.sqrt(Math.pow(this.x1 - this.x3, 2) + Math.pow(this.y1 - this.y3, 2) + Math.pow(this.z1 - this.z3, 2));
+    var angle = Math.acos((Math.pow(side2, 2) + Math.pow(side1, 2) - Math.pow(side3, 2)) / (2 * side1 * side2));
+    var textCord5 = (side1 - side2 * Math.cos(angle)) / side1;
+    var textCord6 = side2 * Math.sin(angle) / side1;
 
     this.texCoords = [
         this.maxS, this.minT,
         this.minS, this.minT,
-        (ab - bc * Math.cos(beta)) / ab, bc * Math.sin(beta) / ab
+        textCord5, textCord6
     ];
 
     this.initialTexCoords = [];
