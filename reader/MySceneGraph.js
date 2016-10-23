08fcb15b30c5a261d2f296e8ad0ef14cc2f196ef
Pedro Costa <pedrodc23@gmail.com>
2016-10-23 11:44:53 +0100

    Updated angle error
     : Angle was in rad and it must be specified in deg

--- reader/MySceneGraph.js
+++ reader/MySceneGraph.js
@@ -11,7 +11,7 @@ function MySceneGraph(filename, scene) {
     this.root;
     this.axis_length;
     //Parser das views
-    this.viewsIndice=0;
+    this.viewsIndice = 0;
     this.perspectives = [];
 
     //Parser das luzes
@@ -122,35 +122,34 @@ MySceneGraph.prototype.parserToViews = function(rootElement) {
 
         //Obter os valores da perspective
         var id = perspective[i].attributes.getNamedItem("id").value;
-        var near = perspective[i].attributes.getNamedItem("near").value*1.0;
-        var far = perspective[i].attributes.getNamedItem("far").value*1.0;
-        var angle = perspective[i].attributes.getNamedItem("angle").value*1.0;
-
+        var near = perspective[i].attributes.getNamedItem("near").value * 1.0;
+        var far = perspective[i].attributes.getNamedItem("far").value * 1.0;
+        var angle = perspective[i].attributes.getNamedItem("angle").value * 1.0;
+        angle = angle * Math.PI / 180;
         //Obter o que está definido dentro de cada perspective (from e to) e obter os valores de estes
         var from = perspective[i].getElementsByTagName('from');
-        var xf=from[0].attributes.getNamedItem("x").value*1.0;
-        var yf=from[0].attributes.getNamedItem("y").value*1.0;
-        var zf=from[0].attributes.getNamedItem("z").value*1.0;
+        var xf = from[0].attributes.getNamedItem("x").value * 1.0;
+        var yf = from[0].attributes.getNamedItem("y").value * 1.0;
+        var zf = from[0].attributes.getNamedItem("z").value * 1.0;
 
         var to = perspective[i].getElementsByTagName('to');
-        var xt=to[0].attributes.getNamedItem("x").value*1.0;
-        var yt=to[0].attributes.getNamedItem("y").value*1.0;
-        var zt=to[0].attributes.getNamedItem("z").value*1.0;
+        var xt = to[0].attributes.getNamedItem("x").value * 1.0;
+        var yt = to[0].attributes.getNamedItem("y").value * 1.0;
+        var zt = to[0].attributes.getNamedItem("z").value * 1.0;
 
-        console.log(id+","+near+","+far+","+angle+","+xf+","+yf+","+zf+","+xt+","+yt+","+zt);
+        console.log(id + "," + near + "," + far + "," + angle + "," + xf + "," + yf + "," + zf + "," + xt + "," + yt + "," + zt);
 
-        var tempCamera=new CGFcamera(angle, near, far,
-            vec3.fromValues(xf,yf,zf),
-            vec3.fromValues(xt,yt,zt));
+        var tempCamera = new CGFcamera(angle, near, far,
+            vec3.fromValues(xf, yf, zf),
+            vec3.fromValues(xt, yt, zt));
 
-        var tempView=new GraphView(id,tempCamera);
+        var tempView = new GraphView(id, tempCamera);
 
         this.perspectives.push(tempView);
 
-        if(id === viewDefault)
-        {
-          console.log("ENTROU"+i);
-          this.viewsIndice=i;
+        if (id === viewDefault) {
+            console.log("ENTROU" + i);
+            this.viewsIndice = i;
         }
 
     }
