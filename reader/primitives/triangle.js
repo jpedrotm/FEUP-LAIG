0974069fad274f7cbf92a5494fee5e1cabeb08b7
Pedro Costa <pedrodc23@gmail.com>
2016-10-23 11:02:29 +0100

    Updated triangle textures

--- reader/primitives/triangle.js
+++ reader/primitives/triangle.js
@@ -15,6 +15,11 @@ function Triangle(scene, x1, y1, z1, x2, y2, z2, x3, y3, z3) {
     this.y3 = y3;
     this.z3 = z3;
 
+    this.minS = 0;
+    this.minT = 0;
+    this.maxS = 1;
+    this.maxT = 1;
+
     this.initBuffers();
 };
 
@@ -41,12 +46,15 @@ Triangle.prototype.initBuffers = function() {
         0, 0, 1
     ];
 
+    var ab = Math.sqrt(Math.pow(this.x2 - this.x1, 2) + Math.pow(this.y2 - this.y1, 2) + Math.pow(this.z2 - this.z1, 2));
+    var bc = Math.sqrt(Math.pow(this.x2 - this.x3, 2) + Math.pow(this.y2 - this.y3, 2) + Math.pow(this.z2 - this.z3, 2));
+    var ac = Math.sqrt(Math.pow(this.x1 - this.x3, 2) + Math.pow(this.y1 - this.y3, 2) + Math.pow(this.z1 - this.z3, 2));
+    var beta = Math.acos((Math.pow(bc, 2) + Math.pow(ab, 2) - Math.pow(ac, 2)) / (2 * ab * bc));
+
     this.texCoords = [
-
-        this.x1, this.y1, this.z1,
-        this.x2, this.y2, this.z2,
-        this.x3, this.y3, this.z3
-
+        this.maxS, this.minT,
+        this.minS, this.minT,
+        (ab - bc * Math.cos(beta)) / ab, bc * Math.sin(beta) / ab
     ];
 
 
