97039f8538cf9706db4e09b27ed14efa4b71594b
Pedro Costa <pedrodc23@gmail.com>
2016-11-21 00:47:31 +0000

    helice animated

--- reader/Animations/circularAnimation.js
+++ reader/Animations/circularAnimation.js
@@ -1,17 +1,17 @@
 function circularAnimation(scene, id, span, type, center, radius, initialAngle, rotationAngle) {
-    Animation.call(this, scene,id, span, type);
+    Animation.call(this, scene, id, span, type);
     this.center = center;
     this.radius = radius;
 
-    this.radAng=Math.PI/180;
+    this.radAng = Math.PI / 180;
 
-    this.initialAngle = initialAngle*this.radAng;
-    this.rotationAngle = rotationAngle*this.radAng;
-    console.log("START ANG: "+this.initialAngle);
-    console.log("ROTATION ANG:"+this.rotationAngle);
+    this.initialAngle = initialAngle * this.radAng;
+    this.rotationAngle = rotationAngle * this.radAng;
+    console.log("START ANG: " + this.initialAngle);
+    console.log("ROTATION ANG:" + this.rotationAngle);
 
-    this.currTime=0;
-    this.currAngle=0;
+    this.currTime = 0;
+    this.currAngle = 0;
     this.x;
     this.y;
     this.z;
@@ -21,49 +21,49 @@ function circularAnimation(scene, id, span, type, center, radius, initialAngle, 
 circularAnimation.prototype = Object.create(Animation.prototype);
 circularAnimation.prototype.constructor = circularAnimation;
 
-circularAnimation.prototype.getAnimationCopy = function(){
-  return new circularAnimation(this.scene,this.id,this.span,this.type,this.center,this.radius,this.initialAngle/this.radAng,this.rotationAngle/this.radAng);
+circularAnimation.prototype.getAnimationCopy = function() {
+    return new circularAnimation(this.scene, this.id, this.span, this.type, this.center, this.radius, this.initialAngle / this.radAng, this.rotationAngle / this.radAng);
 };
 
 circularAnimation.prototype.updateAnimation = function(time) {
 
-  console.log("AQUI");
+    console.log("AQUI");
 
-  this.currTime += time/1000;
+    this.currTime += time / 1000;
 
-  console.log("TIME: "+this.currTime);
+    console.log("TIME: " + this.currTime);
 
-  if (this.currTime >= this.span) {
+    if (this.currTime >= this.span) {
 
-    this.inUse=false;
+        this.inUse = false;
 
-      return;
-  } else {
+        return;
+    } else {
 
-    var percentage=this.currTime/this.span;
+        var percentage = this.currTime / this.span;
 
-    this.currAngle=this.rotationAngle*percentage;
+        this.currAngle = this.rotationAngle * percentage;
 
-  }
-
-};
-
-circularAnimation.prototype.displayAnimation = function(){
-
-  var x =this.radius * Math.sin(this.initialAngle + this.currAngle);
-  var z=this.radius * Math.cos(this.initialAngle + this.currAngle);
-
-  console.log("VARIAVEIS: "+x+", "+z);
-
-  this.scene.translate(x+this.center.x,this.center.y,z+this.center.z);
-  this.scene.rotate(this.initialAngle + this.currAngle, 0, 1, 0);
-  this.scene.rotate(Math.PI/2,0,1,0);
+    }
 
 };
 
-circularAnimation.prototype.resetAnimation = function(){
+circularAnimation.prototype.displayAnimation = function() {
 
-  this.currTime=0;
-  this.currAngle=0;
+    var x = this.radius * Math.sin(this.initialAngle + this.currAngle);
+    var z = this.radius * Math.cos(this.initialAngle + this.currAngle);
+
+    console.log("VARIAVEIS: " + x + ", " + z);
+
+    this.scene.translate(x + this.center.x, this.center.y, z + this.center.z);
+    this.scene.rotate(this.initialAngle + this.currAngle, 0, 1, 0);
+    this.scene.rotate(Math.PI / 2, 0, 1, 0);
+
+};
+
+circularAnimation.prototype.resetAnimation = function() {
+
+    this.currTime = 0;
+    this.currAngle = 0;
 
 };
