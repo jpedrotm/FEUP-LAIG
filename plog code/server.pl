0a62ed95f995937ed7783d01bea506c8bf57ea54
Pedro Costa <pedrodc23@gmail.com>
2016-11-28 11:56:51 +0000

    added includes

--- plog code/server.pl
+++ plog code/server.pl
@@ -1,7 +1,12 @@
 :-use_module(library(sockets)).
 :-use_module(library(lists)).
 :-use_module(library(codesio)).
-
+:-include('MartianChess.pl').
+:-include('Logic.pl').
+:-include('BotLogic.pl').
+:-include('Interface.pl').
+:-include('Utils.pl').
+:-include('Menus.pl').
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 %%%%                                        Server                                                   %%%%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -23,7 +28,7 @@ server :-
 	socket_server_close(Socket),
 	write('Closed Server'),nl.
 
-% Server Loop 
+% Server Loop
 % Uncomment writes for more information on incomming connections
 server_loop(Socket) :-
 	repeat,
@@ -38,22 +43,22 @@ server_loop(Socket) :-
 			close_stream(Stream),
 			fail
 		)),
-		
+
 		% Generate Response
 		handle_request(Request, MyReply, Status),
 		format('Request: ~q~n',[Request]),
 		format('Reply: ~q~n', [MyReply]),
-		
+
 		% Output Response
 		format(Stream, 'HTTP/1.0 ~p~n', [Status]),
 		format(Stream, 'Access-Control-Allow-Origin: *~n', []),
 		format(Stream, 'Content-Type: text/plain~n~n', []),
 		format(Stream, '~p', [MyReply]),
-	
+
 		% write('Finnished Connection'),nl,nl,
 		close_stream(Stream),
 	(Request = quit), !.
-	
+
 close_stream(Stream) :- flush_output(Stream), close(Stream).
 
 % Handles parsed HTTP requests
@@ -69,15 +74,15 @@ handle_request(_, 'Bad Request', '400 Bad Request').
 read_request(Stream, Request) :-
 	read_line(Stream, LineCodes),
 	print_header_line(LineCodes),
-	
+
 	% Parse Request
 	atom_codes('GET /',Get),
 	append(Get,RL,LineCodes),
-	read_request_aux(RL,RL2),	
-	
+	read_request_aux(RL,RL2),
+
 	catch(read_from_codes(RL2, Request), error(syntax_error(_),_), fail), !.
 read_request(_,syntax_error).
-	
+
 read_request_aux([32|_],[46]) :- !.
 read_request_aux([C|Cs],[C|RCs]) :- read_request_aux(Cs, RCs).
 
@@ -109,4 +114,3 @@ parse_input(quit, goodbye).
 
 test(_,[],N) :- N =< 0.
 test(A,[A|Bs],N) :- N1 is N-1, test(A,Bs,N1).
-	
