e73cd0e2e1e81f97ca9187f510db906986a42d73
Pedro Costa <pedrodc23@gmail.com>
2016-12-27 19:08:04 +0000

    bot vs bot

--- reader/Game/game.js
+++ reader/Game/game.js
@@ -4,17 +4,19 @@ function Game(scene){
   this.gameHistory = new GameHistory(scene);
 
   this.playing = 'player1'; //Para saber que jogador faz a jogada
-  this.firstBot = false;
+  this.firstBot = true;
   this.secondBot = true;
   this.firstPlayerPoints=0;
   this.secondPlayerPoints=0;
   this.readyToMakeAMove=0;
   this.currentValidMoves=[];
   this.bot1played=0;
-  this.bot2player=0;
+  this.bot2played=0;
   this.firstCell=new Point2D(-1,-1);
   this.secondCell=new Point2D(-1,-1);
   this.switchTurn = false;
+  this.botCurrentDeltaTime=0;
+  this.botDeltaTime=100;
 
 }
 
@@ -36,16 +38,19 @@ Game.prototype.movePiece=function(bot,xi,yi,xf,yf){
       console.log(this.firstPlayerPoints);
       console.log("player two points:");
       console.log(this.secondPlayerPoints);
-      if(this.playing==='player2')
-      {
-        console.log("player changed");
-        this.playing='player1';
+      if(this.firstBot === false){
+        if(this.playing==='player2')
+        {
+          console.log("player changed");
+          this.playing='player1';
+        }
+        else if(this.playing==='player1')
+        {
+          console.log("player changed");
+          this.playing='player2';
+        }
       }
-      else if(this.playing==='player1')
-      {
-        console.log("player changed");
-        this.playing='player2';
-      }
+
     }
     this.readyToMakeAMove=0;
   }
@@ -71,7 +76,23 @@ Game.prototype.update = function(currTime){
         this.playBot();
     }
   }else{
-    this.playBot();
+    if(this.firstBot === true && this.botCurrentDeltaTime > this.botDeltaTime){
+      if(this.playing==='player2')
+      {
+        console.log("player changed");
+        this.playing='player1';
+      }
+      else if(this.playing==='player1')
+      {
+        console.log("player changed");
+        this.playing='player2';
+      }
+      this.botCurrentDeltaTime=0;
+      this.playBot();
+    }
+    console.log(this.botCurrentDeltaTime);
+    this.botCurrentDeltaTime++;
+
   }
 
 
@@ -99,7 +120,9 @@ Game.prototype.playPlayer = function() {
 };
 
 Game.prototype.playBot = function(){
-  this.bot1played=1;
+  if(this.firstBot ===false && this.secondBot === true){
+    this.bot1played=1;
+  }
   this.readyToMakeAMove=0;
   var tempBoard = this.gameBoard.getBoard();
   var request = 'botPlay([' + tempBoard + '],' + this.playing + ',' + 2 + ')';
@@ -110,6 +133,7 @@ Game.prototype.playBot = function(){
 
 Game.prototype.botMove = function(move){
   var botMove = JSON.parse(move.target.response);
+  console.log(botMove);
   this.readyToMakeAMove=1;
   this.movePiece('bot',botMove[0],botMove[1],botMove[2],botMove[3]);
 };
