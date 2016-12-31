8a72f7191447d276c71f4dbc27b2aa43ab4f3571
Pedro Costa <pedrodc23@gmail.com>
2016-12-31 15:06:27 +0000

    commented code

--- reader/Comunication/comunication.js
+++ reader/Comunication/comunication.js
@@ -1,3 +1,10 @@
+/**
+ * Sends a string request to prolog
+ * @param  {string} requestString string request to be sent to prolog.
+ * @param  {callback} onSuccess     callback function that handles prolog's answer.
+ * @param  {callback} onError       callback function that deals with .
+ * @param  {int} port          port to connect.
+ */
 function getPrologRequest(requestString, onSuccess, onError, port)
 {
   var requestPort = port || 8081;
--- reader/Counters/Counter.js
+++ reader/Counters/Counter.js
@@ -1,3 +1,7 @@
+/**
+ *  Class counter, displays player score
+ *  @param CGFscene
+ */
 function Counter(scene) {
 
     CGFobject.call(this, scene);
@@ -28,6 +32,10 @@ function Counter(scene) {
 Counter.prototype = Object.create(CGFobject.prototype);
 Counter.prototype.constructor = Counter;
 
+/**
+ * Updates the dozens and units counter
+ * @param {int} points amount of points to be added
+ */
 Counter.prototype.add = function(points) {
 
     if ((this.units + points) < 10 && points > 0) {
@@ -44,6 +52,9 @@ Counter.prototype.add = function(points) {
 
 };
 
+/**
+ * displays the counters and deals with their animation
+ */
 Counter.prototype.display = function() {
 
     if (this.animateUnits === true) {
